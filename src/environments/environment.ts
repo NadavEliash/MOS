81bce9b74ba5fdc01fcf5631c0e9c7582f227df5
NadavEliash <nadav.eliash@gmail.com>
2026-01-28 08:12:37 +0200

    prod env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
-const isProd = false;
+const isProd = true;
 export const environment = {
   production: isProd,
   baseUrl: isProd ? 'https://opendata.dev.molsa.gov.il/api/' : 'http://localhost:3000/api/'
