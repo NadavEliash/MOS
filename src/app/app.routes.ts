a3ba6dc38c166b6f1833a32bf98eb31dfdd163cc
NadavEliash <nadav.eliash@gmail.com>
2026-06-28 09:45:09 +0300

    error pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { CategoryComponent } from './pages/category/category.component';
 import { ReportComponent } from './pages/report/report.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
 	{ path: '', component: HomeComponent },
@@ -9,5 +10,5 @@ export const routes: Routes = [
 	{ path: 'category/:id', component: CategoryComponent },
 	{ path: 'category/:id/:graph', component: CategoryComponent },
 	{ path: 'report', component: ReportComponent },
-	{ path: '**', redirectTo: '' }
+	{ path: '**', component: NotFoundComponent }
 ];
